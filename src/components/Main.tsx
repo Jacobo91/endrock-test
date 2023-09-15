d98250e45274e0baae1ebcf834d931d9c2575edd
jacobo91 <jacobovelasquez1991@gmail.com>
2023-09-15 14:45:37 -0500

    add animation to change

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 export default function Main({ children } : {children: React.ReactNode}){
     return(
-        <main className="mx-5 sm:mx-10 mt-10">
+        <motion.div 
+            className="mx-5 sm:mx-10 mt-10"
+            initial={{ opacity:0, y:20 }}
+            animate={{ opacity:1, y:0 }}
+            exit={{ opacity:0, y:20 }}
+        >
             {children}
-        </main>
+        </motion.div>
     )
 }
